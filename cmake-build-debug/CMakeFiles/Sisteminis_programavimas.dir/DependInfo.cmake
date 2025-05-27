e7e9824fd6ea1eb13dd6c16f2f02818a7160d8ca
Baravik419 <ervin.borkovski@gmail.com>
2025-05-27 22:33:50 +0300

    2ND finished

---
+++ cmake-build-debug/CMakeFiles/Sisteminis_programavimas.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/erwin/CLionProjects/Sisteminis_programavimas/1ND/main.cpp" "CMakeFiles/Sisteminis_programavimas.dir/1ND/main.cpp.obj" "gcc" "CMakeFiles/Sisteminis_programavimas.dir/1ND/main.cpp.obj.d"
+  "C:/Users/erwin/CLionProjects/Sisteminis_programavimas/2ND/main.cpp" "CMakeFiles/Sisteminis_programavimas.dir/2ND/main.cpp.obj" "gcc" "CMakeFiles/Sisteminis_programavimas.dir/2ND/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Sisteminis_programavimas.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sisteminis_programavimas.dir/1ND/main.cpp.obj"
+  "CMakeFiles/Sisteminis_programavimas.dir/1ND/main.cpp.obj.d"
+  "CMakeFiles/Sisteminis_programavimas.dir/2ND/main.cpp.obj"
+  "CMakeFiles/Sisteminis_programavimas.dir/2ND/main.cpp.obj.d"
+  "Sisteminis_programavimas.exe"
+  "Sisteminis_programavimas.exe.manifest"
+  "Sisteminis_programavimas.pdb"
+  "libSisteminis_programavimas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sisteminis_programavimas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
