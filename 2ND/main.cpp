2848e17a7df010cc14fba0d8bcb1b37a12ce2a55
Baravik419 <ervin.borkovski@gmail.com>
2025-05-27 22:05:50 +0300

    1 task done

--- 2ND/main.cpp
+++ 2ND/main.cpp
@@ -6,6 +6,31 @@
 
 using namespace std;
 
+// 1 uzduotis, naudojamas FindFirstFile
+
+void SearchForMP3(const std::wstring& catalogue) {
+    wstring searchPath = catalogue + L"\\*.mp3";
+    WIN32_FIND_DATAW data;
+    HANDLE searchKey = FindFirstFileW(searchPath.c_str(), &data);
+
+    if (searchKey == INVALID_HANDLE_VALUE) {
+        wcerr << L"No files found." << std::endl;
+        return;
+    }
+
+    do {
+        wcout << L"Found file: " << data.cFileName << std::endl;
+    } while (FindNextFileW(searchKey, &data));
+
+    FindClose(searchKey);
+}
+
+
+
+
 int main(){
 
+    wstring catalogue = L"C:\\Users\\erwin\\Downloads";
+    SearchForMP3(catalogue);
+    return 0;
 }
