b598a9f91896ede7a88c8597354eb77ea6e0999f
Baravik419 <ervin.borkovski@gmail.com>
2025-05-27 22:18:12 +0300

    2 task done

--- 2ND/main.cpp
+++ 2ND/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <windows.h>
-#include <vector>
+#include <cstdint>
 #include <string>
 #include <memory>
+
+#pragma pack(push, 1)
+struct ID3v1Tag {
+    char tag[3];
+    char title[30];
+    char artist[30];
+    char album[30];
+    char year[4];
+    char comment[30];
+    uint8_t genre;
+};
+#pragma pack(pop)
 
 using namespace std;
 
@@ -25,12 +37,48 @@ void SearchForMP3(const std::wstring& catalogue) {
     FindClose(searchKey);
 }
 
+// 2 Uzduotis
 
+void ReadID3v1Tag(const wstring& filePath) {
+    HANDLE file = CreateFileW(filePath.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
+    if (file == INVALID_HANDLE_VALUE) {
+        wcerr << L"Unsuccessful file opening: " << filePath << endl;
+        return;
+    }
+
+    LARGE_INTEGER fileSize;
+    GetFileSizeEx(file, &fileSize);
+
+    if (fileSize.QuadPart < 128) {
+        wcerr << L"File too small, to have such tag." << endl;
+        CloseHandle(file);
+        return;
+    }
+
+    SetFilePointer(file, -128, NULL, FILE_END);
+
+    ID3v1Tag tag;
+    DWORD bytesRead;
+    if (ReadFile(file, &tag, sizeof(tag), &bytesRead, NULL) && bytesRead == sizeof(tag)) {
+        if (strncmp(tag.tag, "TAG", 3) == 0) {
+            cout << "Dainos pavadinimas: " << string(tag.title, 30) << endl;
+            cout << "Atlikejas: " << string(tag.artist, 30) << endl;
+            cout << "Albumas: " << string(tag.album, 30) << endl;
+            cout << "Metai: " << string(tag.year, 4) << endl;
+        } else {
+            cout << "ID3v1 zyma nerasta." << endl;
+        }
+    }
+
+    CloseHandle(file);
+}
 
 int main(){
 
     wstring catalogue = L"C:\\Users\\erwin\\Downloads";
     SearchForMP3(catalogue);
+    wstring file = L"C:\\Users\\erwin\\Downloads\\bensound-far.mp3";
+    ReadID3v1Tag(file);
     return 0;
 }
